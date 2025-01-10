d270463e36700c1103ab953c46b524f752ef45fe
Mehak <73754277+superstar2020-code@users.noreply.github.com>
2025-01-10 21:56:36 +0530

    first commit

---
+++ server/controllers/userController.js
@@ -0,0 +1,23 @@
+import userModal from "../models/userModal.js";
+
+export const getUserData = async (req, res) =>{
+    try{
+        const {userId} = req.body;
+        const user = await userModal.findById(userId);
+
+        if(!user){
+            return res.json({success: false, message: ' User not found'});
+
+        }
+
+        res.json({
+            success:true,
+            userData: {
+                name: user.name,
+                isAccountVerified: user.isAccountVerified
+            }
+        });
+    } catch(error){
+             res.json({success:false, message: error.message});
+    }
+}
